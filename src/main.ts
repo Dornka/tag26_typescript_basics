54e1852c4a789bd2d9caa74fa6a4b5213e0622dc
FIRST_NAME LAST_NAME <Skiber03@gmail.com>
2023-09-25 16:36:03 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+let age : number = 3;
+
+for (let i : number = 0 ; i < 3; i++) {
+    console.log(i)
+}
+
+if (age > 18) {
+    console.log("Alter ist größer 18")
+}
+else {
+    console.log("Alter kleiner 18")
+}
+
+//
+
+let score : number = 0;
+
+if (score !== 0){
+    console.log("Score ist verfügbar")
+}
+
+if (score){
+    console.log("Score wird als truthy ausgewertet.")
+}
+
+
